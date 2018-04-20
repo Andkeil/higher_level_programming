fe8271b3c0f732a37d9d5326949c336d0d298700
Andrew Keil <monkeyninja147@gmail.com>
2018-04-20 00:41:27 +0000

    Initial

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        row = list(map(lambda x: x**2, row))
+        new_matrix.append(row)
+    return new_matrix
