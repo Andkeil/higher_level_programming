4314c84d68891d24660677ee82b2fd229128f5b1
Andrew Keil <monkeyninja147@gmail.com>
2018-05-01 02:25:06 +0000

    Initial

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        print("Inside result: {}".format(a/b))
+        result = a/b
+    except ZeroDivisionError:
+        print("Inside result: {}".format(None))
+        result = None
+    finally:
+        return result
