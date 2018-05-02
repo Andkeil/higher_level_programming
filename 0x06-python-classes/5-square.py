9d003c0a60d55c502f3bbc64dd3809c3357a91cc
Andrew Keil <monkeyninja147@gmail.com>
2018-05-02 04:23:42 +0000

    pep8

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Square class def
+"""
+
+
 class Square:
+    """
+    Square class with private attribute
+    """
     def __init__(self, size=0):
+        """
+        Arguments:
+        size: size of square
+         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,14 +21,24 @@ class Square:
             self.__size = size
 
     def area(self):
+        """
+        return square of size
+        """
         return (self.__size ** 2)
 
     @property
     def size(self):
+        """
+        return size
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """size: size of square
+        sets value if passes validation
+        raise error otherwise
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -25,5 +47,8 @@ class Square:
             self.__size = value
 
     def my_print(self):
+        """
+        prints out a visual representation of square
+        """
         print("\n".join(["".join(['#' for i in range(self.__size)])
                          for j in range(self.__size)]))
