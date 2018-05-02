6fa88d3ab9d7f03f239e64836a69a6eb61df8a16
Andrew Keil <monkeyninja147@gmail.com>
2018-05-02 03:17:38 +0000

    initial

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        print("\n".join(["".join(['#' for i in range(self.__size)])
+                         for j in range(self.__size)]))
