4bd03e696a94e4599c94e3fda35f0fe0958e99fb
Andrew Keil <monkeyninja147@gmail.com>
2018-05-02 04:18:54 +0000

    pep8

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Square class def
+"""
+
+
 class Square:
+    """
+    Square class with private attribute
+    """
     def __init__(self, size=0):
+        """
+        Arguments:
+        size: size of square
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,4 +21,7 @@ class Square:
             self.__size = size
 
     def area(self):
+        """
+        Return area of square size
+        """
         return (self.__size ** 2)
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+Square class def
+"""
+
+
 class Square:
+    """
+    Square class with private attribute
+    """
     def __init__(self, size=0):
+        """
+        Arguments:
+        size: size of square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,14 +21,24 @@ class Square:
             self.__size = size
 
     def area(self):
+        """
+        return square of size
+        """
         return (self.__size ** 2)
 
     @property
     def size(self):
+        """
+        return size
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """size: size of square
+        sets value if passes validation
+        raise error otherwise
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
