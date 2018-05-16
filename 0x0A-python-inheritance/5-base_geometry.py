d7d6ef4ca18b5ce2d002f3e5ad88a96a2963e068
Andrew Keil <monkeyninja147@gmail.com>
2018-05-16 00:05:07 +0000

    Initial

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class of BaseGeometry
+    """
+    def __init___(self):
+        """
+        method initialize
+        """
+        pass
