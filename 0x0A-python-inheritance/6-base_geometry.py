9fa948fe2944411c21f1ff46b2020b231ef8bcfe
Andrew Keil <monkeyninja147@gmail.com>
2018-05-16 08:44:19 +0000

    fixes

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,11 +8,6 @@ class BaseGeometry:
     """
     Class of BaseGeometry
     """
-    def __init___(self):
-        """
-        method initialize
-        """
-        pass
 
     def area(self):
         """
