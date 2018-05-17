a5b1c25e178944d3a78482d261eb9886d87993a2
Andrew Keil <monkeyninja147@gmail.com>
2018-05-17 02:09:17 +0000

    #13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+student
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        method initialization
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        method dict rep of student
+        """
+        a_dict = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                a_dict[i] = getattr(self, i)
+        return a_dict
+
+    def reload_from_json(self, json):
+        """
+        method reload instance with json
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
