fdc176aff972e45b1221e6f7027170d098967fb6
Andrew Keil <monkeyninja147@gmail.com>
2018-05-17 01:30:10 +0000

    Initial

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    method json rep of string object
+    """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+from_json_string
+"""
+
+
+def from_json_string(my_str):
+    """
+    method object from JSON string
+    """
+    return json.loads(my_str)
