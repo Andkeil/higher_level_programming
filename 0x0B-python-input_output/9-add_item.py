54342f8610ca891506120a86b5bd47177e8ee08d
Andrew Keil <monkeyninja147@gmail.com>
2018-05-17 01:55:01 +0000

    revert and chmod

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,12 +9,10 @@ add_item
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 
-filename = 'add_item.json'
-
 try:
-    new = load_json(filename)
-except (ValueError, FileNotFoundError):
-    new = []
-
-new = new + sys.argv[1:]
-save_to_json_file(new, filename)
+    my_list = load_json("add_item.json")
+except:
+    my_list = []
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, "add_item.json")
