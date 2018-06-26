c0c780e6fce3bee96f753077e903e432c196cadf
Andrew Keil <monkeyninja147@gmail.com>
2018-06-26 03:30:14 +0000

    Missing variable name for ordering

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script list all records of second table with a name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER by DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER by score DESC;
