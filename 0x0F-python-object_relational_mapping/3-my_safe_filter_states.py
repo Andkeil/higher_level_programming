6af8b022aee3b733698313aec14f7246817d1c7c
Andrew Keil <monkeyninja147@gmail.com>
2018-07-20 23:40:29 +0000

    Add back in ordering

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s",(sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id\
+    ",(sys.argv[4],))
     for row in cur:
         print("{}".format(row))
     cur.close()
