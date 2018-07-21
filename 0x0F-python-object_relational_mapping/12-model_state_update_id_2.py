d880629c431ea43dac6abffd976c21131a3b1948
Andrew Keil <monkeyninja147@gmail.com>
2018-07-21 03:21:15 +0000

    cleaning up #12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,9 +11,10 @@ Script change name of State in database
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     Base.metadata.create_all(engine)
-    state_with_id_2 = session().query(State).filter_by(id=2).first()
+    state_with_id_2 = session.query(State).filter_by(id=2).first()
     state_with_id_2.name = "New Mexico"
-    session().commit()
-    session().close()
+    session.commit()
+    session.close()
