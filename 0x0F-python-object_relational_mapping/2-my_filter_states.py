ac90f14958ae75cdc6861685b85facdb39764381
Andrew Keil <monkeyninja147@gmail.com>
2018-07-21 00:05:46 +0000

    ordering fixes and edge cases #2-3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{:s}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name='{:s}' ORDER BY\
+    states.id ASC".format(sys.argv[4]))
     for row in cur:
-        print("{}".format(row))
+        if row[1] == sys.argv[4]:
+            print("{}".format(row))
     cur.close()
     db.close()
