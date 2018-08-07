4a2b96013ad908ad9c38c9e3285bda76cd2d58ed
Andrew Keil <monkeyninja147@gmail.com>
2018-08-07 01:03:11 +0000

    Corrected handling for numbers and blank input #8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,15 +8,15 @@ import sys
 
 
 if __name__ == "__main__":
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    info = {'q': q}
     try:
-        info = {'q': sys.argv[1]}
-        r = requests.post('http://0.0.0.0:5000/search_user', info)
-    except:
-        r = requests.post('http://0.0.0.0:5000/search_user')
-    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', data=info).json()
         if r:
-            print("[{}] {}".format(r.json()["id"], r.json()["name"]))
+            print("[{}] {}".format(r['id'], r['name']))
         else:
             print("No result")
-    except KeyError:
+    except ValueError:
         print("Not a valid JSON")
