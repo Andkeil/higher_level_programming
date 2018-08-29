8e37296c9fddccada2bc4f2956cf13f5a5724fb1
Andrew Keil <monkeyninja147@gmail.com>
2018-08-29 18:32:35 +0000

    Fixed to handle double digit numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  console.log(process.argv.sort().reverse()[1]);
+  const args = [];
+  for (let i = 3; i <= process.argv.length; i++) {
+    args.push(parseInt(process.argv[i - 1]));
+  } console.log(args.sort().reverse()[1]);
 }
