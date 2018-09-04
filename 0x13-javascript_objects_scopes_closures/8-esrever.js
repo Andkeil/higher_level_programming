01eb27eebdc9f7e7d3ee5049907311897af88863
Andrew Keil <monkeyninja147@gmail.com>
2018-09-04 23:59:03 +0000

    Removing var typing

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  var last = list.length - 1;
+  let last = list.length - 1;
   let new_arr = []
   for (let i = last; i >= 0; i--) {
     new_arr.push(list[i]);
