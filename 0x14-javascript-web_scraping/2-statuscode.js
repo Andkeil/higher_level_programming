17fe326fd23317f60269897c2d77abd90efab964
Andrew Keil <monkeyninja147@gmail.com>
2018-09-05 21:52:04 +0000

    semistandard fix and add error handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-let url = process.argv[2]
+let url = process.argv[2];
 let request = require('request');
 
 request(url, (error, response) => {
-  console.log('code: ' + response.statusCode);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
