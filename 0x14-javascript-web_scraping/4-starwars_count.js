85d2d97b9e7d367057610dba310593213538d473
Andrew Keil <monkeyninja147@gmail.com>
2018-09-05 22:34:07 +0000

    Working wedge finder but semistandard issues

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+let url = process.argv[2];
+let request = require('request');
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    let wedgeCount = 0;
+    let movieList = JSON.parse(body).results;
+    for (let movie in movieList) {
+      let characterList = movieList[movie].characters;
+      for (let id in characterList) {
+        if (characterList[id].includes('18')) {
+	  wedgeCount += 1;
+        }
+      }
+    }
+    console.log(wedgeCount);
+  }
+});
