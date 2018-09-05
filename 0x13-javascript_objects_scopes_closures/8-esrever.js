f76b733c5b421e6d39e1ccf7e118a88e6628f69a
Andrew Keil <monkeyninja147@gmail.com>
2018-09-05 00:02:14 +0000

    semistandard camelCase fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let last = list.length - 1;
-  let new_arr = []
+  let newArr = [];
   for (let i = last; i >= 0; i--) {
-    new_arr.push(list[i]);
-    }
-  return new_arr;
+    newArr.push(list[i]);
+  }
+  return newArr;
 };
